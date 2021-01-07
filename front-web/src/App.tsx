b6ba6fc69c876daa4f0e4d6dfbc469233a583222
Fabio Gomes <fabiogms34@gmail.com>
2021-01-07 16:40:24 -0300

    Add Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Navbar from './Navbar';
 
  
 
@@ -6,7 +7,7 @@ function App() {
     
     return (  
        <div>
-       <h1>Hello! </h1>
+       <Navbar />
        </div>
     )
 }
